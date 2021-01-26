2fe71a12c6a7abee858b67a5d56d8666827475f1
Corey Runkel <runkelcorey@gmail.com>
2021-01-26 11:51:58 -0500

    Created new unique, persistent ID

--- split.R
+++ split.R
@@ -12,12 +12,9 @@ library(tidyverse)
 #create sensible unique persistent identifier
 internal <- readWorkbook("internal-cfrt.xlsx", sheet = "Data", detectDates = T) %>% #read database
   separate(`Country(ies)`, into = c("country", NA), remove=FALSE, sep = ",", extra = "drop") %>%
-  group_by(country, `Institutional.Role(s)`, `Adoption/Proposal.Date.(MM-DD-YYYY)`) %>%
-  add_tally() %>% #add number for each entry within country-role-date group, remove subsequent countries
-  ungroup() %>%
   inner_join(codelist[,c(29,32)], by = c("country" = "iso.name.en")) %>% #keep countries that have ISO name equivalents (for naming purposes)
-  mutate(id=paste(countrycode(country, origin="country.name", destination = "iso3c"), substr(`Institutional.Role(s)`, 1, 1), `Adoption/Proposal.Date.(MM-DD-YYYY)`, n, sep = "_")) #construct unique persistent id: country_role_date_number
-
+  add_column(ran = runif(nrow(.))) %>%
+  mutate(id = paste(countrycode(country, origin="country.name", destination = "iso3c"), substr(`Institutional.Role(s)`, 1, 1), `Adoption/Proposal.Date.(MM-DD-YYYY)`, substr(ran, 3, 9), sep = "_")) #construct unique persistent id: country_role_date_number
 #assign people to countries
 assignments <- list("Priya" = c("Argentina", "Chile", "Peru", "Spain"),
                     "Mallory" = c("Australia", "Canada", "South Africa", "United Kingdom"),
