02a7c43879583a2165f5f12a5e0cea4d078da2a7
Corey Runkel <runkelcorey@gmail.com>
2021-02-01 09:42:32 -0500

    Added int'l assignments, made small efficiency tweaks to id generation

--- split.R
+++ split.R
@@ -8,13 +8,34 @@ library(openxlsx)
 library(countrycode)
 library(tidyverse)
 
-
 #create sensible unique persistent identifier
 internal <- readWorkbook("internal-cfrt.xlsx", sheet = "Data", detectDates = T) %>% #read database
-  separate(`Country(ies)`, into = c("country", NA), remove=FALSE, sep = ",", extra = "drop") %>%
-  inner_join(codelist[,c(29,32)], by = c("country" = "iso.name.en")) %>% #keep countries that have ISO name equivalents (for naming purposes)
-  add_column(ran = runif(nrow(.))) %>%
-  mutate(id = paste(countrycode(country, origin="country.name", destination = "iso3c"), substr(`Institutional.Role(s)`, 1, 1), `Adoption/Proposal.Date.(MM-DD-YYYY)`, substr(ran, 3, 9), sep = "_")) #construct unique persistent id: country_role_date_number
+  mutate(code = countrycode(ifelse(`Country(ies)` == "Other (please specify)", `Country(ies).Other`, gsub("[,:].*", "", `Country(ies)`)), origin = "country.name.en", destination = "iso3c", #convert country names to ISO 3-char codes
+                            custom_match = c("EU" = "EUR", #add CFRT-specific interveners
+                                             "African Development Bank" = "BAD",
+                                             "Asian Development Bank" = "ADB",
+                                             "Asian Infrastructure Investment Bank" = "AII",
+                                             "Bank for International Settlements" = "BIS",
+                                             "Development Bank of Latin America" = "CAF",
+                                             "Inter-American Development Bank" = "IDB",
+                                             "International Accounting Standards Board" = "ASB",
+                                             "International Monetary Fund" = "IMF",
+                                             "International Organization of Securities Commissions" = "SCO",
+                                             "Nordic Investment Bank" = "NIB",
+                                             "Paris Club" = "PAR",
+                                             "World Bank Group" = "WBG",
+                                             "Plata Basin Financial Development Fund" = "FON",
+                                             "Central American Bank for Economic Integration (CABEI)" = "CEI",
+                                             "European Bank for Reconstruction and Development" = "ERD",
+                                             "Financial Stability Board" = "FSB",
+                                             "Council of Europe Development Bank" = "CEB",
+                                             "Bank of Central African States" = "XAF",
+                                             "Central Bank of West African States" = "XOF",
+                                             "Micronesia" = "FSM",
+                                             "Kosovo" = "KOS",
+                                             "Palestine" = "PAL"))) %>%
+  mutate(id = paste(code, substr(`Institutional.Role(s)`, 1, 1), `Adoption/Proposal.Date.(MM-DD-YYYY)`, substr(ResponseID, 3, 6), sep = "_")) #construct unique persistent id: country-code_role_date_random-string
+
 #assign people to countries
 assignments <- list("Priya" = c("Argentina", "Chile", "Peru", "Spain"),
                     "Mallory" = c("Australia", "Canada", "South Africa", "United Kingdom"),
@@ -25,7 +46,11 @@ assignments <- list("Priya" = c("Argentina", "Chile", "Peru", "Spain"),
                     "Alex" = c("Egypt", "Qatar", "Saudi Arabia", "Turkey", "United Arab Emirates"),
                     "Junko" = c("India", "Japan", "Korea", "Pakistan", "Russia"),
                     "Adam" = c("Indonesia", "Malaysia", "Philippines", "Thailand"),
-                    "Steven" = c("Mexico", "United States"))
+                    "Steven" = c("Mexico", "United States"),
+                    "Lily" = c("EU", "African Development Bank", "Asian Development Bank", "Bank for International Settlements", "Development Bank of Latin America", "Inter-American Development Bank",
+                               "International Accounting Standards Board", "International Monetary Fund", "International Organization of Securities Commissions", "Nordic Investment Bank", "Paris Club",
+                               "Plata Basin Financial Development Fund", "Central American Bank for Economic Integration (CABEI)", "European Bank for Reconstruction and Development",
+                               "Financial Stability Board", "Council of Europe Development Bank"))
 
 #create blank workbook
 cfrt <- createWorkbook()
@@ -33,5 +58,5 @@ cfrt <- createWorkbook()
 #create function to make one sheet
 for (i in names(assignments)) {
   addWorksheet(cfrt, i)
-  writeData(cfrt, i, filter(internal, country %in% assignments[[i]]), colNames = T) #create a sheet named i filled with entries assigned to i, and keep the column names
+  writeData(cfrt, i, filter(internal, gsub("[,:].*", "", `Country(ies)`) %in% assignments[[i]]), colNames = T) #create a sheet named i filled with entries assigned to i, and keep the column names
 }
